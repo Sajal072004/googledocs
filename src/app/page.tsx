6706018afed8eabb1e499f173e36724a35780a2d
Sajal Namdeo <sajalnamdeo627@gmail.com>
2025-02-15 12:07:11 +0530

    Add Tiptap editor and update document navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>Click Me</Button>
+      Click&nbsp;
+      <Link href={"/documents/123"}>
+        <span className="text-blue-500 underline">Here</span>
+      </Link>
+      &nbsp;to go to document id
     </div>
-  )
-}
+  );
+};
 
 export default Home;
